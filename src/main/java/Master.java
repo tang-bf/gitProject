704e2afff3abc26d685371ac8961b46b4590e77d
tang-bf <1535710733@qq.com>
2020-09-14 21:32:48 +0800

    master

---
+++ src/main/java/Master.java
@@ -0,0 +1,18 @@
+/**
+ * @ProjectName: testfit
+ * @Package: PACKAGE_NAME
+ * @ClassName: Master
+ * @Description:
+ * @Author: tbf
+ * @CreateDate: 2020-09-14 21:28
+ * @UpdateUser: Administrator
+ * @UpdateDate: 2020-09-14 21:28
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
+
+public class Master {
+    public static void main(String[] args) {
+        System.out.println("master主干");
+    }
+}
